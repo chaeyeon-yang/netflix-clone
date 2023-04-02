6e99e41a4a88c7fd5dce2b90b2b8d7c03817ce24
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-04-02 16:30:56 +0900

    Feat: 추가 api 정보 가져오기 ✨

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://api.themoviedb.org/3",
-  params: {
-    api_key= "0761d5a201d824ec542d3308f6e844bf",
-    language: "ko-KR"
-  }
-})
+    baseURL: "https://api.themoviedb.org/3",
+    params: {
+        api_key: "0761d5a201d824ec542d3308f6e844bf",
+        language: "ko-KR",
+    },
+});
 
 export default instance;
