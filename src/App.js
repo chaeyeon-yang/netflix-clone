b73dbddfe03c76c7cbcc0e795a8a1423d06a232a
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-04-07 18:20:21 +0900

    Feat: 변경 React Router Dom 적용 ✨

--- src/App.js
+++ src/App.js
@@ -1,40 +1,36 @@
+import React from "react";
 import "./App.css";
 import Nav from "./components/Nav";
-import Banner from "./components/Banner";
-import Row from "./components/Row";
-import requests from "./api/requests";
 import Footer from "./components/Footer";
+import { Outlet, Routes, Route } from "react-router";
+import MainPage from "./pages/MainPage";
+import DetailPage from "./pages/DetailPage";
+import SearchPage from "./pages/SearchPage";
 
-function App() {
+const Layout = () => {
     return (
-        <div className="App">
+        <div>
             <Nav />
-            <Banner />
-            <Row
-                title="NETFLIX ORIGINALS"
-                id="NO"
-                fetchUrl={requests.fetchNetflixOriginals}
-                isLargeRow
-            />
-            <Row
-                title="Trending Now"
-                id="TN"
-                fetchUrl={requests.fetchTrending}
-            />
-            <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
-            <Row
-                title="Action Movies"
-                id="AM"
-                fetchUrl={requests.fetchActionMovies}
-            />
-            <Row
-                title="Comedy Movies"
-                id="CM"
-                fetchUrl={requests.fetchComedyMovies}
-            />
+
+            <Outlet />
+
             <Footer />
         </div>
     );
-}
+};
+
+const App = () => {
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<MainPage />} />
+                    <Route path=":movieId" element={<DetailPage />} />
+                    <Route path="search" element={<SearchPage />} />
+                </Route>
+            </Routes>
+        </div>
+    );
+};
 
 export default App;
